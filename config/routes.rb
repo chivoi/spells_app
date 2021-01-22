0d07fca9473956002d34dfe08cf312daaf4247bb
chivoi <a.lastovirya@gmail.com>
2021-01-22 14:36:40 +1100

    added delete button to show page, changed behaviour to redirect to index on delete and updated tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'pages#index'
 
-  get '/spells', to: 'spells#index', as: 'spell'
+  get '/spells', to: 'spells#index', as: 'spells'
   post '/spells', to: 'spells#create'
-  get '/spells/:id', to: 'spells#show'
+  get '/spells/:id', to: 'spells#show', as: 'spell'
   put '/spells/:id', to: 'spells#update'
   delete '/spells/:id', to: 'spells#destroy'
   
--- spec/requests/spells_request_spec.rb
+++ spec/requests/spells_request_spec.rb
@@ -51,11 +51,10 @@ RSpec.describe "Spells", type: :request do
   end
 
   describe 'delete /spells/:id' do
-    it 'should respond with json given correct id' do
+    it 'should delete and redirect when given a valid id' do
       put '/spells/4', :params => {name: 'Teleport', description: 'You end up somewhere else', type: 'transportation'}
       delete '/spells/4'
-      expect(response).to have_http_status(200) 
-      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to redirect_to('/spells')
     end
     it 'should respond with 404 when given invalid id' do
       delete '/spells/99'
